dbc7c0f576bb4d05536c07cefa7e086c377b5cef
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-09-29 13:58:11 -0400

    feat: Add the display module with the TODO markers.

---
+++ iterator/iterator/display.py
@@ -0,0 +1,26 @@
+"""Print values and lists of values."""
+
+from typing import List
+
+
+def convert_bool_to_answer(argument: bool):
+    """Return a string-based and human-readable representation of a bool."""
+    # TODO: Return a string-based and human-readable version of a bool
+    # TODO: For instance, if argument is False then return "No"
+    # TODO: For instance, if argument is True then return "Yes"
+
+
+def display_list(values: List, indent=""):
+    """Display the provided list when iterating and printing every indented value."""
+    # TODO: Iterate through all of the values inside of the list,
+    # displaying them in the following fashion as an example:
+    # 2**0 = 4
+    # 2**1 = 8
+    # 2**2 = 16
+    # 2**3 = 32
+    # 2**4 = 64
+    # 2**5 = 128
+    # 2**6 = 256
+    # 2**7 = 512
+    # TODO: ensure that the output is indented by the numbers of spaces in the ident
+    # TODO: the display should be of the format: {indent}2**{index} = {value}
