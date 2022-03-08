fc72e1689e003f8dea4e69e60ca5565b94180b5e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-03-08 10:54:21 -0500

    fix: Use the correct comments in TODO markers in the display module.

--- iterator/iterator/display.py
+++ iterator/iterator/display.py
@@ -13,14 +13,8 @@ def convert_bool_to_answer(argument: bool):
 def display_list(values: List, indent=""):
     """Display the provided list when iterating and printing every indented value."""
     # TODO: Iterate through all of the values inside of the list,
-    # displaying them in the following fashion as an example:
-    # 2**0 = 4
-    # 2**1 = 8
-    # 2**2 = 16
-    # 2**3 = 32
-    # 2**4 = 64
-    # 2**5 = 128
-    # 2**6 = 256
-    # 2**7 = 512
-    # TODO: ensure that the output is indented by the numbers of spaces in the ident
+    # displaying them in the following fashion as show in the project
+    # description on the Proactive Programmers web site:
+    # https://proactiveprogrammers.com/discrete-structures/engineering-efforts/iterative-exponentiation/
+    # TODO: ensure that the output is indented by the numbers of spaces in the indent
     # TODO: the display should be of the format: {indent}2**{index} = {value}
